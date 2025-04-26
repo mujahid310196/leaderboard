e720e44966284fd21d143773c0f9202f26d89813
mujahid <bhoraniyamujahid@gmail.com>
2025-04-26 15:35:53 +0530

    redme changed

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -5,6 +5,7 @@
     <title>Leaderboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-dark text-white">
 
 <div class="container mt-5">
